14c6d5e2c4efac80cb1b57bef7dd29b66e1d7a40
ndcuong <ndcuong912@gmail.com>
2025-08-01 20:43:22 +0700

    test

--- src/components/main/page-share-layout.jsx
+++ src/components/main/page-share-layout.jsx
@@ -2,7 +2,7 @@ export default function PageLayout({left, center, end}){
     return(
         <>
         {<aside className=" hidden lg:block toc">{left}</aside>}
-         <main className="m-auto col-span-3 flex flex-col gap-[32px] items-center sm:items-start border border-[var(--neutral-0)] p-6 bg-[var(--neutral-100)] shadow-sm w-full max-w-3xl mb-5">
+         <main className=" col-span-3 flex flex-col gap-[32px] items-center sm:items-start border border-[var(--neutral-0)] p-6 bg-[var(--neutral-100)] shadow-sm w-full max-w-3xl mb-5 mx-auto">
             {center}
         </main>
         {<aside className="hidden lg:inline-block">{end}</aside>}
